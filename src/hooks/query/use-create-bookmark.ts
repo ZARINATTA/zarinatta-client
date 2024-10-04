1cbe092ade1990fd97030f71a4acd6818f0bcd7a
kyw0716 <kyw0716@naver.com>
2024-10-04 21:26:28 +0900

    refactor: 즐겨찾기 요청 수정

--- src/hooks/query/use-create-bookmark.ts
+++ src/hooks/query/use-create-bookmark.ts
@@ -1,10 +1,14 @@
 import { API_END_POINT } from '@/static/api';
 import { BookmarkRequestParams } from '@/type';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
 
 export const useCreateBookmarkMutation = () =>
   useMutation({
     mutationFn: (requestParams: BookmarkRequestParams) =>
-      axios.post(`${API_END_POINT}/v1/bookmark/create`, requestParams, { withCredentials: true }),
+      fetch(`${API_END_POINT}/v1/bookmark/create`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(requestParams),
+        credentials: 'include',
+      }),
   });
